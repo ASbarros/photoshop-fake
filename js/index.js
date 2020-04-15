7990c82c9b63783bae915024c4e110f2e0e3a114
asbarros <anderson.barros0298@gmail.com>
2020-04-15 05:45:57 -0300

    filtro de log

--- js/index.js
+++ js/index.js
@@ -39,6 +39,34 @@ $('#negativo').on('click', function (e) {
     img.src = map.data
 })
 
+$('#log').on('click', function (e) {
+    if (page != 'imagem.html') {
+        page = 'imagem.html'
+        carrega(page, 'root')
+    }
+    const img = document.getElementById('img');
+
+    //cria um canvas invisível
+    const canvas = document.getElementById('canvas');
+    canvas.width = img.width;
+    const context = canvas.getContext('2d');
+    //cria um canvas invisível
+
+    //desenha a imagem no canvas
+    context.drawImage(img, 0, 0);
+
+    //recupera vetor de cores
+    var map = context.getImageData(0, 0, img.width, img.height);
+    for (let i = 0; i < map.data.length; i += 4) {
+        map.data[i] = 105.886 * Math.log10(1 + map.data[i]);
+        map.data[i + 1] = 105.886 * Math.log10(1 + map.data[i + 1]);
+        map.data[i + 2] = 105.886 * Math.log10(1 + map.data[i + 2]);
+      }
+    context.putImageData(map, 0, 0)
+
+    img.src = map.data
+})
+
 $('#input').on('change', function (e) {
     carrega('imagem.html', 'root')
     setTimeout(function () {
