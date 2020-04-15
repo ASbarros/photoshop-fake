a49082d1242d9e38f0996bb1e4968133ce2d8b5c
asbarros <anderson.barros0298@gmail.com>
2020-04-15 08:40:11 -0300

    colocando o log inverso

--- js/index.js
+++ js/index.js
@@ -61,7 +61,35 @@ $('#log').on('click', function (e) {
         map.data[i] = 105.886 * Math.log10(1 + map.data[i]);
         map.data[i + 1] = 105.886 * Math.log10(1 + map.data[i + 1]);
         map.data[i + 2] = 105.886 * Math.log10(1 + map.data[i + 2]);
-      }
+    }
+    context.putImageData(map, 0, 0)
+
+    img.src = map.data
+})
+
+$('#loginverso').on('click', function (e) {
+    if (page != 'imagem.html') {
+        page = 'imagem.html'
+        carrega(page, 'root')
+    }
+    const img = document.getElementById('img');
+
+    //cria um canvas invisível
+    const canvas = document.getElementById('canvas');
+    canvas.width = img.width;
+    const context = canvas.getContext('2d');
+    //cria um canvas invisível
+
+    //desenha a imagem no canvas
+    context.drawImage(img, 0, 0);
+
+    //recupera vetor de cores
+    var map = context.getImageData(0, 0, img.width, img.height);
+    for (let i = 0; i < map.data.length; i += 4) {
+        map.data[i] = Math.pow(10, map.data[i] / 105.886);
+        map.data[i + 1] = Math.pow(10, map.data[i + 1] / 105.886);
+        map.data[i + 2] = Math.pow(10, map.data[i + 2] / 105.886);
+    }
     context.putImageData(map, 0, 0)
 
     img.src = map.data
